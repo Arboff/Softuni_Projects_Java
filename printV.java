6630f0f5afd260cd048a4830126a25abdaf76016
Nikola Arbov <49321625+Arboff@users.noreply.github.com>
2021-11-07 17:28:52 +0300

    Paulina's exam

---
+++ printV.java
@@ -0,0 +1,40 @@
+package com.company.Softuni_Projects_Java;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class printV {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int input = Integer.parseInt(scan.nextLine());
+        String five = "*****";
+        String indent = "";
+        String between = "*   *";
+        String end = "********";
+        String start = "";
+        for(int i = 0; i < (input - 2) * 2; i++){
+            indent += " ";
+        }
+        System.out.println(five + indent + five);
+        System.out.println();
+
+        for (int i = 0; i < input - 2; i++ ){
+            indent=indent.replaceFirst(String.valueOf(indent.charAt(0)),"");
+            indent=indent.replaceFirst(String.valueOf(indent.charAt(0)),"");
+            start += " ";
+
+
+
+            System.out.println(start + between + indent + between );
+            System.out.println();
+        }
+        start += " ";
+        System.out.println(start + end);
+
+
+
+
+
+    }
+}
